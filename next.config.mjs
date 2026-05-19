7b0ac6beb61cbc327eea368cb8530f6ac0d4f057
transrinko <transrinko@gmail.com>
2026-05-19 15:36:38 +0700

    update proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/backend/:path*',
+        destination: 'http://103.193.145.61:7777/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
