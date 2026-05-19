d774c779484fb85fe806c540ef897a9ab83dc998
transrinko <transrinko@gmail.com>
2026-05-19 16:24:33 +0700

    update mjs to js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/backend/:path*',
+        destination: 'http://103.193.145.61:7777/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
