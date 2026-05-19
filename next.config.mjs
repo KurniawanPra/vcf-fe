f8d6392d3636f572fa012af00a9a3eecf41bea09
transrinko <transrinko@gmail.com>
2026-05-19 15:50:29 +0700

    fix: remove /api from backend base URL to avoid duplication

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/backend/:path*',
-        destination: 'http://103.193.145.61:7777/:path*',
+        destination: 'http://103.193.145.61:7777/api/:path*',
       },
     ];
   },
