40684533525ce28004ac3876b7933ccde4dcfd29
transrinko <transrinko@gmail.com>
2026-05-19 16:12:58 +0700

    use vercel.json for rewrites instead of next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/backend/:path*',
-        destination: 'http://103.193.145.61:7777/api/:path*',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
