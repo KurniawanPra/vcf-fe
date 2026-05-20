0df02aedfc3b9c9e15094abd3798d8a08bb29ab0
Rinko <ayamsaya85@gmail.com>
2026-05-20 13:30:04 +0700

    Update next.config.js for standalone output
    Add standalone output configuration to Next.js config.

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/backend/:path*',
-//         destination: 'http://103.193.145.61:7777/api/:path*',
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
-
 const BACKEND_URL = process.env.BACKEND_URL;
 
 const nextConfig = {
+  output: 'standalone', // ← tambahkan ini
   async rewrites() {
     return [
       {
