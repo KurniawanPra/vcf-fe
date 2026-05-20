f88c5f5bed07d390f37a5e1fb607f60977443ee9
transrinko <transrinko@gmail.com>
2026-05-20 11:54:54 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/backend/:path*',
+//         destination: 'http://103.193.145.61:7777/api/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
+
+const BACKEND_URL = process.env.BACKEND_URL;
+
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/backend/:path*',
-        destination: 'http://103.193.145.61:7777/api/:path*',
+        destination: `${BACKEND_URL}/api/:path*`,
       },
     ];
   },
