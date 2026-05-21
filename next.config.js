8f0604d7aab8ec7519a56b834de1d2bd8252c5d8
transrinko <transrinko@gmail.com>
2026-05-21 08:45:13 +0700

    fix: hardcode backend URL in next.config.js to fix rewrite not working

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const BACKEND_URL = process.env.BACKEND_URL;
-
 const nextConfig = {
   output: 'standalone',
   async rewrites() {
     return [
       {
         source: '/backend/:path*',
-        destination: `${BACKEND_URL}/api/:path*`,
+        destination: 'http://103.193.145.61:7777/api/:path*',
       },
     ];
   },
