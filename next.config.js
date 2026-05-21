32ba555ce18e8256b935d1048a60870a940bf78d
transrinko <transrinko@gmail.com>
2026-05-21 10:36:43 +0700

    konfigurasi next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/backend/:path*',
-        destination: 'http://103.193.145.61:7777/api/:path*',
+        source: `${process.env.PATH_URL}/:path*`,
+        destination: `${process.env.API_URL}/api/:path*`,
       },
     ];
   },
