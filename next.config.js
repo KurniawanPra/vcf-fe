e4f9c2aedb34fdc8a29d30d68c8313f2a9a042eb
transrinko <transrinko@gmail.com>
2026-06-01 16:45:32 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -26,9 +26,6 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  workboxOptions: {
-    disableDevLogs: true,
-  },
   runtimeCaching: [
     {
       // Cache API calls - network first
